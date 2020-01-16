c7c515845380cdd7d711484993538c6b644f46f0
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 19:32:44 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-print('hello')
-print('world')
-print('hello')
+#inside class Time:
+
+def __add__(self, other):
+    if isinstance(other, Time):
+         return self.add_time(other)
+    else:
+        return self.increment(other)
+      
+     def add_time(self, other):
+         seconds = self.time_to_int() + other.time_to_int()
+         return int_to_time(seconds)
+        
+     def increment(self,seconds):
+         seconds += self.time_to_int()
+         return int_to_time(seconds)
